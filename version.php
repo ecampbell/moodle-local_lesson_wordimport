32f88752c8ad8df9050aed2aa555b5a50489682d
Eoin Campbell <campbeeo@tcd.ie>
2025-04-24 16:49:04 +0100

    Support Moodle 3.9 upwards

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2025042400;              // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2023041800;               // Requires Moodle 4.2 or higher.
+$plugin->requires = 2020060900;               // Requires Moodle 3.9 or higher.
 $plugin->component = 'local_lesson_wordimport';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.3 (Build: 2025042400)'; // Human readable version information.
